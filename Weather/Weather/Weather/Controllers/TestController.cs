ff8dfc1c37ecaaeb6ba50b23c0ec783079e35740
studentutuDev <studentutu@gmail.com>
2021-06-05 16:21:09 +0300

    fixed routes and using options for mvc / initial commit

---
+++ Weather/Weather/Weather/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Weather.Controllers
+{
+    [ApiController]
+    public class TestController: Controller
+    {
+        [HttpGet("api/user")]
+        public IActionResult Get()
+        {
+
+           return Ok(new {name="Nick"});
+            
+        }
+    }
+}
